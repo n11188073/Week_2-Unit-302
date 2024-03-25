99f7a547eacc639ba76fb4beba278094aa610851
Bernie <n11188073@qut.edu.au>
2024-03-25 11:44:28 +1000

    Added User Class and IAuthenticationService

---
+++ src/User.java
@@ -0,0 +1,28 @@
+public class User {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+
+}
